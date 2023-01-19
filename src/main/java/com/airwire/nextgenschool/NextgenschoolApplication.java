99169d93172772275680405261091fef6a32acf7
balajivs <balaji.sambhale33@gmail.com>
2023-01-19 13:09:51 +0530

    Added Docker File

---
+++ src/main/java/com/airwire/nextgenschool/NextgenschoolApplication.java
@@ -0,0 +1,17 @@
+package com.airwire.nextgenschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Balaji Sambhale
+ */
+@SpringBootApplication
+public class NextgenschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextgenschoolApplication.class, args);
+	}
+
+	
+}
